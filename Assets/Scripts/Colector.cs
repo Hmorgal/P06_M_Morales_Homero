62f6ffd38b756183cf0896f5e45b2d827d38f6fc
Hmorgal <hmorgal025@g.educaand.es>
2024-11-14 14:41:24 +0100

    Tercera subida

---
+++ Assets/Scripts/Colector.cs
@@ -0,0 +1,54 @@
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Colector : MonoBehaviour
+{
+    int CONTADOR = 10 ;
+
+    public GameObject itemFinal;
+    public GameObject caja;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        itemFinal.SetActive(false);
+        caja.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+        void OnTriggerEnter(Collider other){
+
+        if (other.CompareTag("Item")){
+
+            Destroy(other.gameObject);
+
+            CONTADOR --;
+
+            if (CONTADOR == 0){
+
+                itemFinal.SetActive(true);
+                caja.SetActive(false);
+
+            }
+
+        }
+
+        if (other.CompareTag("Finish")){
+
+            Destroy(other.gameObject);
+
+            SceneManager.LoadScene("SampleScene");
+
+        }
+
+        
+
+    }
+}
